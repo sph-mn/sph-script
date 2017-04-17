8a58dfb20c92fce980e33298fab25eb9c9e64b78
sph <sph@posteo.eu>
2017-04-17 17:45:44 +0000

    rewrite name-add and similar and rename it to tag-add

---
+++ guile-modules/sph/filesystem/tag.scm
@@ -0,0 +1,97 @@
+(library (sph filesystem tag)
+  (export
+    tag-add
+    tag-default-config
+    tag-group
+    tag-group-from-search
+    tag-name->tags-and-extensions&
+    tag-remove
+    tag-replace
+    tag-sort)
+  (import
+    (sph common)
+    (sph two))
+
+  ;add-to-tag-group-from-search
+  ;add-to-tag-group-from-list-of-files
+  ;add, remove, sort tags - considering mime-extensions
+
+  (define-as tag-default-config alist-q
+    extensions (get-mime-extensions-cached) delimiter #\. ignore-extensions? #f)
+
+  (define* (tag-name->tags-and-extensions& a c #:key (config tag-default-config))
+    (let
+      ( (extensions (alist-q-ref config extensions))
+        (words (string-split a (alist-q-ref config delimiter))))
+      (consecutive& (l (a) (contains? extensions a)) (reverse words)
+        (l (extensions tags) (c (reverse tags) (reverse extensions))))))
+
+  (define* (tag-create-name tags extensions #:key (config tag-default-config))
+    (string-append (string-join tags (string (alist-q-ref config delimiter)))
+      (string-join extensions "." (q prefix))))
+
+  (define (prepare-tags& a c) (let (a (remove string-null? a)) (if (not (null? a)) (c a))))
+
+  (define (prepare-tags-each-path& tags paths c)
+    (prepare-tags& tags (l (tags-new) (each (l (path) (c tags-new path)) paths))))
+
+  (define* (tag-add tags paths #:key (config tag-default-config))
+    (prepare-tags-each-path& tags paths
+      (l (tags-new path)
+        (let (name-old (basename path))
+          (tag-name->tags-and-extensions& name-old
+            (l (tags-old extensions)
+              (let
+                (name-new
+                  (tag-create-name (list-set-union tags-old tags-new) extensions #:config config))
+                (if (not (string-equal? name-old name-new))
+                  (rename-file path
+                    (get-unique-target-path (string-append (dirname path) "/" name-new)))))))))))
+
+  (define (not-null? a) (not (null? a)))
+
+  (define* (tag-remove tags paths #:key (config tag-default-config))
+    (prepare-tags-each-path& tags paths
+      (l (tags path)
+        (let (name-old (basename path))
+          (tag-name->tags-and-extensions& name-old
+            (l (tags-old extensions)
+              (if (not-null? tags-old)
+                (let*
+                  ( (tags (complement tags-old tags))
+                    (tags (if (null? tags) (list (first tags-old)) tags)))
+                  (let (name-new (tag-create-name tags extensions #:config config))
+                    (if (not (string-equal? name-old name-new))
+                      (rename-file path
+                        (get-unique-target-path (string-append (dirname path) "/" name-new)))))))))))))
+
+  (define (tag-sort-tags a) (delete-duplicates-sorted (list-sort string< a)))
+
+  (define* (tag-replace tags-replace tags-replacement paths #:key (config tag-default-config))
+    (prepare-tags-each-path& tags-replacement paths
+      (l (tags-replacement path)
+        (let (name-old (basename path))
+          (tag-name->tags-and-extensions& name-old
+            (l (tags-old extensions)
+              (let
+                (name-new
+                  (tag-create-name
+                    (list-set-union (complement tags-old tags-replace) tags-replacement) extensions
+                    #:config config))
+                (if (not (string-equal? name-old name-new))
+                  (rename-file path
+                    (get-unique-target-path (string-append (dirname path) "/" name-new)))))))))))
+
+  (define* (tag-sort paths #:key (config tag-default-config))
+    (each
+      (l (path)
+        (let (name-old (basename path))
+          (tag-name->tags-and-extensions& name-old
+            (l (tags-old extensions)
+              (let (tags-new (tag-sort-tags tags-old))
+                (if (not-null? tags-new)
+                  (let (name-new (tag-create-name tags-new extensions #:config config))
+                    (if (not (string-equal? name-old name-new))
+                      (rename-file path
+                        (get-unique-target-path (string-append (dirname path) "/" name-new)))))))))))
+      paths)))
