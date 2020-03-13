53db1dda0e7dd49604d69bec70e8e7b20a2951f4
sph <sph@posteo.eu>
2020-03-13 15:31:33 +0100

    updates for current sph-lib version

--- guile-modules/sph/media-info.scm
+++ guile-modules/sph/media-info.scm
@@ -1,50 +1,39 @@
-(library (sph media-info)
-  (export
-    media-info
-    media-info-video)
-  (import
-    (guile)
-    (json parser)
-    (sph)
-    (sph alist)
-    (sph filesystem)
-    (sph hashtable)
-    (sph io)
-    (sph list)
-    (sph number)
-    (sph other)
-    (sph process)
-    (sph string)
-    (sph vector))
+(define-module (sph media-info))
 
-  (define sph-media-info-description
-    "analyse media files and get information like resolution and bitrate.
-     depends on ffmpeg")
+(use-modules (json parser) (sph)
+  (sph alist) (sph filesystem)
+  (sph hashtable) (sph io)
+  (sph list) (sph number) (sph other) (sph process) (sph string) (sph vector))
 
-  (define (media-info path)
-    (hash-map->list pair
-      (json-string->scm
-        (string-trim-right
-          (execute->string "ffprobe" "-v" "quiet" "-print_format" "json" "-show_format" path)))))
+(export media-info media-info-video)
 
-  (define (hash-table->alist a)
-    (hash-map->list
-      (l (key value) (pair key (if (hash-table? value) (hash-table->alist value) value))) a))
+(define sph-media-info-description
+  "analyse media files and get information like resolution and bitrate.
+   depends on ffmpeg")
 
-  (define (json-result->list a)
-    (if (hash-table? a) (hash-map->list (l (key value) (pair key (json-result->list value))) a)
-      (if (list? a) (map json-result->list a) a)))
+(define (media-info path)
+  (hash-map->list pair
+    (json-string->scm
+      (string-trim-right
+        (execute->string "ffprobe" "-v" "quiet" "-print_format" "json" "-show_format" path)))))
 
-  (define (media-info-video path)
-    (let*
-      ( (data
-          (json-result->list
-            (json-string->scm
-              (string-trim-right
-                (execute->string "ffprobe" "-v" "quiet" "-print_format" "json" "-show_streams" path))))))
-      (if (and data (not (null? data)))
-        (first
-          (filter
-            (l (a) (string-equal? "video" (alist-ref a "codec_type")))
-            (vector->list (alist-ref data "streams"))))
-        #f))))
+(define (hash-table->alist a)
+  (hash-map->list
+    (l (key value) (pair key (if (hash-table? value) (hash-table->alist value) value))) a))
+
+(define (json-result->list a)
+  (if (hash-table? a) (hash-map->list (l (key value) (pair key (json-result->list value))) a)
+    (if (list? a) (map json-result->list a) a)))
+
+(define (media-info-video path)
+  (let*
+    ( (data
+        (json-result->list
+          (json-string->scm
+            (string-trim-right
+              (execute->string "ffprobe" "-v" "quiet" "-print_format" "json" "-show_streams" path))))))
+    (if (and data (not (null? data)))
+      (first
+        (filter (l (a) (string-equal? "video" (alist-ref a "codec_type")))
+          (vector->list (alist-ref data "streams"))))
+      #f)))
